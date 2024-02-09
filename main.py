c754a7fd82eeb6530d84c6de52063e8fff072a0f
Max Edison <amirhossein.heidari.email@gmail.com>
2024-02-10 02:43:55 +0330

    function 'XOR() added. (performs XOR operation on two 8-bit Binary values, returns result)'

--- main.py
+++ main.py
@@ -11,6 +11,21 @@ def str2bin(string):
 def bin2int(binary):
     return int(binary, 2)
 
+def XOR(msgByte, keyByte):
+    
+
+
+    if len(msgByte) != 8 or len(keyByte) != 8 or not all(bit in '01' for bit in msgByte) or not all(bit in '01' for bit in keyByte):
+        print("System ERROR [ERR CODE : 9]")
+        return 9
+
+    value1 = int(msgByte, 2)
+    value2 = int(keyByte, 2)
+
+    result = value1 ^ value2
+    result_binary = format(result, '08b')
+    return result_binary
+
 
 message = input("Enter Message: ")
 key = input("Enter Key: ")
